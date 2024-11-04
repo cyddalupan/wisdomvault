a29d194d63370464c05801913bcc8b2e58dbe91a
cyd dalupan <cydmdalupan@gmail.com>
2024-11-04 06:34:27 +0000

    Create crud for answer

--- answer/admin.py
+++ answer/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import QuestionAnswer
 
-# Register your models here.
+class QuestionAnswerAdmin(admin.ModelAdmin):
+    # Define the fields to display in the admin
+    list_display = ('title', 'question', 'answer')
+
+    # Specify the fields to be displayed in the form
+    fields = ('title', 'question', 'answer')
+
+    # Make the 'answer' field read-only
+    readonly_fields = ('answer',)
+
+# Register the model and the custom admin class
+admin.site.register(QuestionAnswer, QuestionAnswerAdmin)
