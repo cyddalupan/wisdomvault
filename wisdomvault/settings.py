893ccb25514244be6ad3f93d3a3239746360903f
cyd dalupan <cydmdalupan@gmail.com>
2024-11-04 04:06:14 +0000

    add jazzmin and fix admin

--- wisdomvault/settings.py
+++ wisdomvault/settings.py
@@ -37,12 +37,14 @@ ALLOWED_HOSTS = ["*", "https://wisdomvault.welfareph.com/"]
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
 ]
 
 MIDDLEWARE = [
