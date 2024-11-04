ce74f6472c176e3c76eb2b0ba7e82c0f179d66fe
cyd dalupan <cydmdalupan@gmail.com>
2024-11-04 07:49:28 +0000

    AI answers

--- answer/admin.py
+++ answer/admin.py
@@ -1,15 +1,44 @@
 from django.contrib import admin
 from .models import QuestionAnswer
+from dotenv import load_dotenv
+from openai import OpenAI
+from django.utils.text import Truncator
+load_dotenv()
+
+client = OpenAI()
 
 class QuestionAnswerAdmin(admin.ModelAdmin):
     # Define the fields to display in the admin
-    list_display = ('title', 'question', 'answer')
+    list_display = ('title', 'short_question', 'short_answer')
 
     # Specify the fields to be displayed in the form
-    fields = ('title', 'question', 'answer')
+    fields = ('title', 'question', 'formatted_answer')
 
     # Make the 'answer' field read-only
-    readonly_fields = ('answer',)
+    readonly_fields = ('formatted_answer', 'answer')
+
+    # Function to truncate and display short version of question
+    def short_question(self, obj):
+        return Truncator(obj.question).chars(50)
+    short_question.short_description = 'Question'
+
+    # Function to truncate and display short version of answer
+    def short_answer(self, obj):
+        return Truncator(obj.answer).chars(50)
+    short_answer.short_description = 'Answer'
+
+    def save_model(self, request, obj, form, change):
+        messages = [
+            {"role": "system", "content": 'Utilize Markdown format. Give Direct Answer'},
+        ]
+        messages.append({"role": "user", "content": obj.question})
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+        )
+        response_content = completion.choices[0].message.content
+        obj.answer = response_content
+        super().save_model(request, obj, form, change)
 
 # Register the model and the custom admin class
 admin.site.register(QuestionAnswer, QuestionAnswerAdmin)
--- answer/models.py
+++ answer/models.py
@@ -1,9 +1,16 @@
+import markdown
+from django.utils.safestring import mark_safe
 from django.db import models
 
 class QuestionAnswer(models.Model):
     title = models.CharField(max_length=255)
     question = models.TextField()
     answer = models.TextField(editable=False) 
+
+    def formatted_answer(self):
+        """Converts the markdown text to HTML and returns it as a safe string."""
+        html = markdown.markdown(self.answer, extensions=['fenced_code', 'codehilite'])
+        return mark_safe(html)
 
     def __str__(self):
         return self.title
