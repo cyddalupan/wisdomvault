ca8c772a18d60196713c7ac3b61407128b49485e
cyd dalupan <cydmdalupan@gmail.com>
2024-11-04 08:05:45 +0000

    Add multiple Users

--- answer/admin.py
+++ answer/admin.py
@@ -3,26 +3,29 @@ from .models import QuestionAnswer
 from dotenv import load_dotenv
 from openai import OpenAI
 from django.utils.text import Truncator
+from django.contrib.auth.models import User
+from django.db import models
+
 load_dotenv()
 
 client = OpenAI()
 
+# Assuming you have a model like
+# class QuestionAnswer(models.Model):
+#     title = models.CharField(max_length=100)
+#     question = models.TextField()
+#     answer = models.TextField()
+#     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
 class QuestionAnswerAdmin(admin.ModelAdmin):
-    # Define the fields to display in the admin
     list_display = ('title', 'short_question', 'short_answer')
-
-    # Specify the fields to be displayed in the form
     fields = ('title', 'question', 'formatted_answer')
-
-    # Make the 'answer' field read-only
     readonly_fields = ('formatted_answer', 'answer')
 
-    # Function to truncate and display short version of question
     def short_question(self, obj):
         return Truncator(obj.question).chars(50)
     short_question.short_description = 'Question'
 
-    # Function to truncate and display short version of answer
     def short_answer(self, obj):
         return Truncator(obj.answer).chars(50)
     short_answer.short_description = 'Answer'
@@ -38,7 +41,15 @@ class QuestionAnswerAdmin(admin.ModelAdmin):
         )
         response_content = completion.choices[0].message.content
         obj.answer = response_content
+
+        if not change:
+            obj.created_by = request.user
         super().save_model(request, obj, form, change)
 
-# Register the model and the custom admin class
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        # Filter only QuestionAnswer objects created by current user
+        return qs.filter(created_by=request.user)
+
+# Register the model and the admin class
 admin.site.register(QuestionAnswer, QuestionAnswerAdmin)
--- answer/models.py
+++ answer/models.py
@@ -1,11 +1,16 @@
+from django.conf import settings  # Import settings to refer to the user model
+from django.db import models
 import markdown
 from django.utils.safestring import mark_safe
-from django.db import models
 
 class QuestionAnswer(models.Model):
     title = models.CharField(max_length=255)
     question = models.TextField()
-    answer = models.TextField(editable=False) 
+    answer = models.TextField(editable=False)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
 
     def formatted_answer(self):
         """Converts the markdown text to HTML and returns it as a safe string."""
