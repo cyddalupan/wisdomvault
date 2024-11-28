6689b04645441473266d77b6e04781d457053c61
cyd dalupan <cydmdalupan@gmail.com>
2024-11-28 08:12:09 +0000

    Fix multiple board

--- kanbanapp/admin.py
+++ kanbanapp/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Board, Column, Task
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'column', 'created_at')
+    list_filter = ('column',)
+    ordering = ('column',)
 
-# Register your models here.
+    def changelist_view(self, request, extra_context=None):
+        boards = Board.objects.all()
+        columns = Column.objects.select_related('board').all()  # Fetch columns with their boards
+        tasks = self.model.objects.select_related('column__board').all()  # Fetch tasks with related columns and boards
+
+        board_data = {
+            board: {
+                'columns': columns.filter(board=board),
+                'column_tasks': {
+                    column.name: tasks.filter(column=column)
+                    for column in columns.filter(board=board)
+                }
+            }
+            for board in boards
+        }
+
+        extra_context = extra_context or {}
+        extra_context['board_data'] = board_data
+
+        return super(TaskAdmin, self).changelist_view(request, extra_context=extra_context)
+
+class ColumnAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+class BoardAdmin(admin.ModelAdmin):
+    list_display = ('name',)  # Remove 'created_at' since it's not a field
+
+admin.site.register(Column, ColumnAdmin)
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Task, TaskAdmin)
--- kanbanapp/models.py
+++ kanbanapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Column(models.Model):
+    name = models.CharField(max_length=100)
+    board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='columns')
+    position = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    column = models.ForeignKey(Column, on_delete=models.CASCADE, related_name='tasks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
