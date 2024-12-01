3314e459bb664d7ff63f316dad71dfbc2aee7e7e
Cyd Dalupan <cydmdalupan@gmail.com>
2024-12-01 07:53:33 +0000

    fix column exclusive 2

--- kanbanapp/admin.py
+++ kanbanapp/admin.py
@@ -73,30 +73,32 @@ class ColumnForm(forms.ModelForm):
         fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        request = kwargs.pop('request', None)
+        # Pop the request out of kwargs so it doesn't interfere with the superclass __init__
+        self.request = kwargs.pop('request', None)
         super(ColumnForm, self).__init__(*args, **kwargs)
 
-        if request:
-            self.fields['board'].queryset = Board.objects.filter(user=request.user)
+        if self.request:
+            # Ensure that only the boards created by the current user are visible
+            self.fields['board'].queryset = Board.objects.filter(user=self.request.user)
 
 class ColumnAdmin(admin.ModelAdmin):
-    form = ColumnForm
     list_display = ('name', 'board')
-    readonly_fields = ('user',)
+    readonly_fields = ('user',) 
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        form.request = request
-        return form
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'board':
+            # Filter the queryset for the board field to only include boards owned by the current user
+            kwargs['queryset'] = Board.objects.filter(user=request.user)
+        return super(ColumnAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
 
     def get_queryset(self, request):
-        qs = super().get_queryset(request)
+        qs = super(ColumnAdmin, self).get_queryset(request)
         return qs.filter(user=request.user)
 
     def save_model(self, request, obj, form, change):
         if not obj.pk:
             obj.user = request.user
-        super().save_model(request, obj, form, change)
+        super(ColumnAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(Column, ColumnAdmin)
 admin.site.register(Board, BoardAdmin)
