ffcf3304747adccb2993146f1308023426d38277
Cleddrence Dalupan <cydmdalupan@gmail.com>
2025-05-01 09:03:19 +0000

    fix error

---
+++ page/migrations/0012_fix_schedule_fields.py
@@ -0,0 +1,9 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("page", "0011_remove_facebookpage_is_schedule"),
+    ]
+
+    operations = [
+    ]
